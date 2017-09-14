a4c86e4f760f491bfad449ea58e26fef49758dff
dominik.mozny <private>
2017-09-14 20:43:24 +0200

    Add reaction on POST Message

--- src/main/java/cz/dominik/artr/Controller.java
+++ src/main/java/cz/dominik/artr/Controller.java
@@ -2,6 +2,8 @@ package cz.dominik.artr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,14 @@ public class Controller {
         answers.add("le");
         json.add("answers", answers);
         return json.toString();
+    }
+
+    @RequestMapping(value = "/postAnswer", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<String> abc(@RequestBody Answer answer) {
+        System.out.println(answer.getWord());
+        JsonObject json = new JsonObject();
+        json.addProperty("word", "greetings from server");
+        return ResponseEntity.ok(json.toString());
     }
 
     public static void main(String[] args) throws Exception {
