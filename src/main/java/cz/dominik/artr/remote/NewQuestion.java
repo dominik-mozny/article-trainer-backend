89c4fcedbb7dca5b969fcd7750fea4a586d69990
dominik.mozny <dominik.mozny@adesso.ch>
2017-10-10 17:56:36 +0200

    Added object for obtaining new questions via REST

---
+++ src/main/java/cz/dominik/artr/remote/NewQuestion.java
@@ -0,0 +1,25 @@
+package cz.dominik.artr.remote;
+
+/**
+ * @author dominik.mozny
+ */
+public class NewQuestion {
+    private String question;
+    private String rightAnswer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getRightAnswer() {
+        return rightAnswer;
+    }
+
+    public void setRightAnswer(String rightAnswer) {
+        this.rightAnswer = rightAnswer;
+    }
+}
---
+++ src/main/java/cz/dominik/artr/remote/NewQuestions.java
@@ -0,0 +1,14 @@
+package cz.dominik.artr.remote;
+
+import java.util.List;
+
+/**
+ * @author dominik.mozny
+ */
+public class NewQuestions {
+    private List<NewQuestion> questions;
+
+    public List<NewQuestion> getQuestions() {
+        return questions;
+    }
+}
