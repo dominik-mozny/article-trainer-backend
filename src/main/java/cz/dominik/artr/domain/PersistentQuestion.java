7e18f968cce5ba8c5715adde26b7e1260c365e85
dominik.mozny <dominik.mozny@adesso.ch>
2017-10-12 16:27:08 +0200

    extended Persistent questions with another useful attributes

--- src/main/java/cz/dominik/artr/domain/PersistentQuestion.java
+++ src/main/java/cz/dominik/artr/domain/PersistentQuestion.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 import cz.dominik.artr.remote.NewQuestion;
 
@@ -16,13 +17,23 @@ public class PersistentQuestion {
     private String question;
     private String rightAnswer;
     private List<String> answers;
-    private List<AnswerStatistic> answerStatistics = new ArrayList<>();
+    private List<AnswerStatistic> answerStatistics;
+    private String collection;
+    @Indexed
+    private int noOfAnswers;
+
+    public PersistentQuestion() {
+
+    }
 
     public PersistentQuestion(long id, NewQuestion newArticleQuestion, QuestionType questionType) {
         this.id = id;
         question = newArticleQuestion.getQuestion();
         rightAnswer = newArticleQuestion.getRightAnswer();
         answers = questionType.getArticles();
+        answerStatistics = new ArrayList<>();
+        this.collection = questionType.toString();
+        noOfAnswers = 0;
     }
 
     public long getId() {
@@ -43,5 +54,13 @@ public class PersistentQuestion {
 
     public List<String> getAnswers() {
         return answers;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public int getNoOfAnswers() {
+        return noOfAnswers;
     }
 }
